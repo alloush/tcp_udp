014666ad0bbd1051021f663ab08b22213cdb1360
damicoac <damicoac@gmail.com>
2012-04-27 18:15:08 -0400

    deleted unneeded header file. write sudo code for ftpClient

---
+++ ftpClient.c
@@ -0,0 +1,18 @@
+/*
+* Aaron D'Amico
+* April 27, 2012
+* 678 TCP over UDP project 
+*/
+
+/*
+* import headers for semaphores, shared memory, printing output, file io
+*/
+
+int main (int argc, char *argv[])
+{
+	/*
+	* set up ints for return values of semaphores, shared memory, and files
+	*/
+	
+
+}
